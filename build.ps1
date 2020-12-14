aa31f484d843aca7c0d646ed2c8cb895c09bee7e
Olivier Lefebvre <olivier.lefebvre@live.com>
2020-12-14 11:48:30 +0100

    ci: update package version

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 $fileversion = "$env:SemVer.0"
 $path = (Get-Location).Path
 
-dotnet pack -c Release -p:PackageVersion=$env:SemVer -p:OutputPath=$path\artifacts -p:FileVersion=$fileversion
+dotnet pack -c Release -p:PackageVersion=$env:Version -p:OutputPath=$path\artifacts -p:FileVersion=$fileversion
