5f3c15788f430c95ab8a60b386e6805c41210615
Olivier Lefebvre <olivier.lefebvre@live.com>
2022-11-22 09:25:56 +0100

    build: drop is4
    BREAKING CHANGES: drop IS4

--- build.ps1
+++ build.ps1
@@ -2,4 +2,3 @@ $fileversion = "$env:SemVer.0"
 $path = (Get-Location).Path
 
 dotnet pack .\TheIdServer.Duende\Template.csproj -c Release -p:PackageVersion=$env:Version -p:OutputPath=$path\artifacts -p:FileVersion=$fileversion
-dotnet pack .\TheIdServer.IS4\Template.csproj -c Release -p:PackageVersion=$env:Version -p:OutputPath=$path\artifacts -p:FileVersion=$fileversion
